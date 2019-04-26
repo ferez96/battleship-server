7a60b916c6408426d9f9f7172ee4a182bb25ed42
Duong Thai Minh <minh9a4@gmail.com>
2019-04-26 23:30:58 +0700

    update Position & PosPool, use short instead of int

--- src/main/java/com/hauduepascal/ferez96/battleship/controller/Position.java
+++ src/main/java/com/hauduepascal/ferez96/battleship/controller/Position.java
@@ -5,24 +5,17 @@ import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 
+
+/**
+ * Self pooling Position class.
+ * Accept: 1 <= x <= 50, 1 <= y <= 50
+ *
+ * @author ferez
+ */
 public class Position {
-    public static final Position ZERO = new Position(0, 0);
-    private static final Pool POOL = new Pool();
-    public final int x;
-    public final int y;
-
-    private Position(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public static Position get(int x, int y) {
-        return POOL.get(x, y);
-    }
-
-    private static class Pool {
-        private static final Logger Log = LoggerFactory.getLogger(Pool.class);
-        private final HashMap<Integer, HashMap<Integer, Position>> POOL = new HashMap<>();
+    private static class PosPool {
+        private static final Logger Log = LoggerFactory.getLogger(Position.PosPool.class);
+        private final HashMap<Short, HashMap<Short, Position>> POOL = new HashMap<>();
         private long size = 0;
 
         private boolean contains(int x, int y) {
@@ -33,24 +26,56 @@ public class Position {
         }
 
         private Position pooling(Position pos) {
-            int x = pos.x;
-            int y = pos.y;
-            Log.trace(String.format("Create new object Position(%d, %d) in pool %s, size=%s", x, y, this.hashCode(), ++this.size));
-            HashMap<Integer, Position> row = POOL.getOrDefault(x, new HashMap<>());
-            row.put(y, pos);
-            POOL.put(x, row);
+            this.size++;
+            Log.trace(String.format("Create new object Position(%d, %d) in pool %s, size=%s", pos.x, pos.y, this.hashCode(), size));
+            HashMap<Short, Position> row = POOL.getOrDefault(pos.x, new HashMap<>());
+            row.put(pos.y, pos);
+            POOL.put(pos.x, row);
             return pos;
         }
 
         private Position get(int x, int y) {
-            if (x <= 0 || y <= 0) return null;
+            if (x <= 0 || y <= 0 || x > 50 || y > 50) return null;
             if (contains(x, y)) return POOL.get(x).get(y);
-            else return pooling(new Position(x, y));
+            else return pooling(new Position((short) x, (short) y));
         }
+    }
+
+    static private final PosPool Pool = new PosPool();
+    static public final Position ZERO = new Position((short) 0, (short) 0); // Special (0,0) position
+    public final short x;
+    public final short y;
+
+    private Position(short x, short y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Position get(int x, int y) {
+        return Pool.get(x, y);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Position position = (Position) o;
+
+        if (x != position.x) return false;
+        return y == position.y;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) x;
+        result = 31 * result + (int) y;
+        return result;
     }
 
     @Override
     public String toString() {
-        return String.format("(%2d,%2d)", x, y);
+        return "(" + x + y + ")";
     }
 }
